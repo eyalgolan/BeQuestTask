fd6f82ac300bafff7b1569c76832cca00f42ddb9
Eyal Golan <eyal@papercup.com>
2022-09-30 18:40:33 +0100

    :white_check_mark: cover all create test cases

--- internal/rest_utils/business_logic/create_test.go
+++ internal/rest_utils/business_logic/create_test.go
@@ -9,37 +9,97 @@ import (
 	"testing"
 )
 
-type inputAndExpectedResult struct {
-	inputAnswer        rest_utils.Answer
-	expectedStatusCode int
-	expectedErr        error
-}
-
-func TestCreate(t *testing.T) {
-	basicInputAnswer := rest_utils.Answer{
+var (
+	basicAnswer = rest_utils.Answer{
 		Event: "create",
 		Data: rest_utils.AnswerData{
 			Key:   "name",
 			Value: "test",
 		},
 	}
+	emptyAnswer = rest_utils.Answer{
+		Event: "create",
+		Data: rest_utils.AnswerData{
+			Key:   "",
+			Value: "",
+		},
+	}
+	notCreateAnswer = rest_utils.Answer{
+		Event: "something",
+		Data: rest_utils.AnswerData{
+			Key:   "",
+			Value: "",
+		},
+	}
+)
+
+func TestCreateBasicCases(t *testing.T) {
+	var tests = []struct {
+		inputAnswer        rest_utils.Answer
+		expectedStatusCode int
+		expectedErr        error
+	}{
+		{basicAnswer,
+			http.StatusCreated,
+			nil,
+		},
+		{
+			emptyAnswer,
+			http.StatusCreated,
+			nil,
+		},
+		{
+			notCreateAnswer,
+			http.StatusBadRequest,
+			ErrNotCreate,
+		},
+	}
+	for _, tt := range tests {
+		testName := fmt.Sprintf("answer: \n%+v\n expected status code: \n%d\n expected error: \n%+v\n",
+			tt.inputAnswer,
+			tt.expectedStatusCode,
+			tt.expectedErr)
+		dbClient, sqlConnection, err := test_utils.Setup()
+		if err != nil {
+			err = test_utils.TearDown(dbClient, sqlConnection)
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Fatal(err)
+		}
+		t.Run(testName, func(t *testing.T) {
+			resultStatusCode, resultErr := Create(tt.inputAnswer, *dbClient)
+			if !errors.Is(resultErr, tt.expectedErr) {
+				t.Errorf("got %+v, want %+v", resultErr, tt.expectedErr)
+			}
+			if resultStatusCode != tt.expectedStatusCode {
+				t.Errorf("got %d, want %d", resultStatusCode, tt.expectedStatusCode)
+			}
+		})
+		err = test_utils.TearDown(dbClient, sqlConnection)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+type inputAndExpectedResult struct {
+	inputAnswer        rest_utils.Answer
+	expectedStatusCode int
+	expectedErr        error
+}
+
+func TestCreateDuplicate(t *testing.T) {
 	var tests = []struct {
 		inputsAndResults []inputAndExpectedResult
 	}{
 		{
 			[]inputAndExpectedResult{{
-				inputAnswer:        basicInputAnswer,
-				expectedStatusCode: http.StatusCreated,
-				expectedErr:        nil,
-			}},
-		},
-		{
-			[]inputAndExpectedResult{{
-				inputAnswer:        basicInputAnswer,
+				inputAnswer:        basicAnswer,
 				expectedStatusCode: http.StatusCreated,
 				expectedErr:        nil,
 			}, {
-				inputAnswer:        basicInputAnswer,
+				inputAnswer:        basicAnswer,
 				expectedStatusCode: http.StatusBadRequest,
 				expectedErr:        ErrDuplicateAnswer,
 			},
