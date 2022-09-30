b708214870efab94dd949b5535acfac785e83418
Eyal Golan <eyal@papercup.com>
2022-09-30 20:04:26 +0100

    :bug: add missing err handeling

--- internal/db_utils/postgress_utils/delete_answer.go
+++ internal/db_utils/postgress_utils/delete_answer.go
@@ -3,10 +3,17 @@ package postgress_utils
 import (
 	"github.com/eyalgolan/key-value-persistent-store/internal/rest_utils"
 	"github.com/pkg/errors"
+	"gorm.io/gorm"
 )
 
 func (c *DBClient) DeleteAnswer(answer rest_utils.AnswerData) error {
 	latestAnswer, err := c.GetLatestAnswer(answer.Key)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return errors.Wrap(err, "Not found")
+		}
+		return errors.Wrap(err, "Unable to perform db action")
+	}
 	latestAnswer.Deleted = true
 	err = c.DB.Save(&latestAnswer).Error
 	if err != nil {
